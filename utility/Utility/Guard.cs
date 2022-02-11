37a272243e1e4686f25bf68ed7fb328acd3067f1
asanjabi <asanjabi@users.noreply.github.com>
2022-02-11 13:40:44 -0800

    some clean up and added guard class

---
+++ utility/Utility/Guard.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Utility
+{
+    public static class Guard
+    {
+        public static T NotNull<T>([NotNull] T obj, string name, ILogger? logger = null)
+        {
+            if (obj == null)
+            {
+                string message = $"{name} can not be null";
+                logger?.LogError(message);
+                throw new ArgumentNullException(name, message);
+            }
+            return obj;
+        }
+
+        public static string NotNullOrEmpty([NotNull] string? obj, string name, ILogger? logger = null)
+        {
+
+            if (string.IsNullOrEmpty(obj))
+            {
+                string message = $"{name} can not be null or empty";
+                logger?.LogError(message);
+                throw new ArgumentException(message, name);
+            }
+            return obj;
+        }
+    }
+}
